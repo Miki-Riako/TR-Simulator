18a6374bb067a8466e6ef38cfa0ce02bb115146a
Miki-Riako <miki_riako@qq.com>
2024-06-25 09:46:11 +0800

    add basic turing machine

--- gui/about.py
+++ gui/about.py
@@ -1,68 +1,10 @@
-from PySide6.QtCore import (QCoreApplication, QMetaObject, Qt)
-from PySide6.QtGui import QFont
-from PySide6.QtWidgets import (QApplication, QLabel, QVBoxLayout, QFrame, QWidget, QSpacerItem, QSizePolicy)
+from PySide6.QtWidgets import QApplication, QFrame
 
 class About(QFrame):
     def __init__(self, text: str, parent=None):
         super().__init__(parent=parent)
-        self.setupUi(self)
+        self.setupUi()
         self.setObjectName(text.replace(' ', '-'))
 
-    def setupUi(self, Form):
-        if not Form.objectName():
-            Form.setObjectName(u"Form")
-        Form.resize(400, 300)  # Set initial window size
-        Form.setMaximumSize(600, 400)  # Set maximum window size
-        self.verticalLayout = QVBoxLayout(Form)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-
-        spacerItemTop = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItemTop)
-
-        self.label = QLabel(Form)
-        self.label.setObjectName(u"label")
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label.setWordWrap(True)  # Enable word wrap
-        self.label.setFont(QFont("Arial", 12))  # Set font size
-        self.verticalLayout.addWidget(self.label)
-
-        self.label_2 = QLabel(Form)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setWordWrap(True)  # Enable word wrap
-        self.label_2.setFont(QFont("Arial", 10))  # Set font size
-        self.verticalLayout.addWidget(self.label_2)
-
-        self.label_3 = QLabel(Form)
-        self.label_3.setObjectName(u"label_3")
-        self.label_3.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label_3.setWordWrap(True)  # Enable word wrap
-        self.label_3.setFont(QFont("Arial", 12))  # Set font size
-        self.verticalLayout.addWidget(self.label_3)
-
-        self.label_4 = QLabel(Form)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setWordWrap(True)  # Enable word wrap
-        self.label_4.setFont(QFont("Arial", 10))  # Set font size
-        self.verticalLayout.addWidget(self.label_4)
-
-        self.label_5 = QLabel(Form)
-        self.label_5.setObjectName(u"label_5")
-        self.label_5.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label_5.setWordWrap(True)  # Enable word wrap
-        self.label_5.setFont(QFont("Arial", 12))  # Set font size
-        self.verticalLayout.addWidget(self.label_5)
-
-        self.label_6 = QLabel(Form)
-        self.label_6.setObjectName(u"label_6")
-        self.label_6.setWordWrap(True)  # Enable word wrap
-        self.label_6.setFont(QFont("Arial", 10))  # Set font size
-        self.verticalLayout.addWidget(self.label_6)
-
-        spacerItemBottom = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItemBottom)
-
-        self.retranslateUi(Form)
-        QMetaObject.connectSlotsByName(Form)
-
-    def retranslateUi(self, Form):
+    def setupUi(self):
         ...
